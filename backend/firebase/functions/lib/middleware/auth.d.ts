d693743ecaab7f8f5be6cd606fcd2d319f8f9560
Guanming Liao <guanming.liao@gmail.com>
2026-02-26 13:51:06 +0800

    refactor: remove deprecated Firebase functions and auth middleware files

--- backend/firebase/functions/lib/middleware/auth.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export interface AuthenticatedRequest extends Request {
-    uid?: string;
-    email?: string;
-}
-export declare function requireAuth(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void>;
